fa8a23c12e59dfe41ac2b4a9ff601133ca6f18d6
Stof95 <christoffeldoorman@hotmail.com>
2018-01-09 13:39:28 +0100

    update

---
+++ project/javascript/index.js
@@ -0,0 +1,15 @@
+/**
+ * index.js
+ *
+ * Programmeerproject
+ * Minor Programmeren (UvA)
+ * Author: Christoffel Doorman
+ * Student number: 10580557
+ *
+ * This file
+ */
+
+// set margin, width and height
+var margin = {top: 50, right: 30, bottom: 30, left: 40},
+    width = 560 - margin.right - margin.left,
+    height = 400 - margin.top - margin.bottom;
