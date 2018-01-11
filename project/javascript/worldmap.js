b865bd09815c107d50efa83e11a5dd96ebd3c98f
Stof95 <christoffeldoorman@hotmail.com>
2018-01-11 13:16:44 +0100

    update

---
+++ project/javascript/worldmap.js
@@ -0,0 +1,22 @@
+/*
+index.js
+
+Programmeerproject
+Minor Programmeren (UvA)
+Author: Christoffel Doorman
+Student number: 10580557
+
+This file contains two functions: a function that builds and updates the
+worldmap, and a function that builds the legend.
+*/
+
+function drawWorldmap(){
+  var map = new Datamap({
+          element: document.getElementById('container'),
+          done: function(datamap) {
+              datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+                  alert(geography.properties.name);
+              });
+          }
+      });
+};
